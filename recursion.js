720bbdfd0e6292b5375db3d34421fbb1e133cea3
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2015-03-14 16:28:14 -0400

    finished recursion exercise

---
+++ recursion.js
@@ -0,0 +1,10 @@
+function reduce(arr, fn, initial) {
+	if(arr.length === 0){
+		return initial;
+	}	
+	else {
+		return reduce(arr.slice(1), fn, fn(initial, arr[0]));
+	}
+}
+
+module.exports = reduce
