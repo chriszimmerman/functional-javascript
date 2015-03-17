a804c9deac138f46e308ac9ed432bee31354d1b9
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2015-03-16 21:54:37 -0400

    refactored first partial solution

--- partial1.js
+++ partial1.js
@@ -1,10 +1,6 @@
 function logger(namespace) {
-	return function foo(){
-		var args = [namespace];
-		for(var i = 0; i < foo.arguments.length; i++){
-			args.push(foo.arguments[i]);
-		}
-		console.log.apply(null, args);
+	return function(){
+		console.log.apply(console, [namespace].concat(Array.prototype.slice.call(arguments)));
 	};
 }
 
