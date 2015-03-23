9d0f7aa8fd9eefecca4ba59365159ce32af5b636
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2015-03-23 18:21:43 -0400

    completed trampline exercise

--- trampoline.js
+++ trampoline.js
@@ -1,12 +1,17 @@
 function repeat(operation, num) {
-	// Modify this so it doesn't cause a stack overflow!
-	if (num <= 0) return
-	operation()
-	return repeat(operation, --num)
+	function repeat_r(operation, num) {
+		if (num <= 0) return;
+		operation();
+		return repeat_r.bind(null, operation, num - 1); 
+	}
+	return trampoline(repeat_r.bind(null, operation, num));
 }
 
 function trampoline(fn) {
-	// You probably want to implement a trampoline!
+	while(fn && fn instanceof Function){
+		fn = fn.apply(fn.context, fn.args);
+	}
+	return fn;
 }
 
 module.exports = function(operation, num) {
